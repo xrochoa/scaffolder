bc9f4bd053f4655853f5fd30554b3dc1da0508e3
Xavier Reyes <xareyesochoa@gmail.com>
2016-02-21 16:25:59 -0800

    Updated ga.js

--- templates/ga.js
+++ templates/ga.js
@@ -1,5 +1,7 @@
-window.ga = function() { ga.q.push(arguments) };
+window.ga = function() {
+    ga.q.push(arguments);
+};
 ga.q = [];
-ga.l = +new Date;
+ga.l = new Date();
 ga('create', 'UA-XXXXX-Y', 'auto');
 ga('send', 'pageview');
